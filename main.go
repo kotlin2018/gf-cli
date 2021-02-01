c4b514cb5a70c6ea552390ab2746e32587cb3e01
LO2016 <56008171+kotlin2018@users.noreply.github.com>
2021-02-01 12:53:06 +0800

    Update main.go
    新增new指令，用于创建封装了基础功能的GF项目

--- main.go
+++ main.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/gogf/gf-cli/commands/env"
-	"github.com/gogf/gf-cli/commands/mod"
-	"github.com/gogf/gf/errors/gerror"
-	"github.com/gogf/gf/os/genv"
-	"strings"
-
 	_ "github.com/gogf/gf-cli/boot"
 	"github.com/gogf/gf-cli/commands/build"
 	"github.com/gogf/gf-cli/commands/docker"
+	"github.com/gogf/gf-cli/commands/env"
 	"github.com/gogf/gf-cli/commands/fix"
 	"github.com/gogf/gf-cli/commands/gen"
 	"github.com/gogf/gf-cli/commands/get"
 	"github.com/gogf/gf-cli/commands/initialize"
 	"github.com/gogf/gf-cli/commands/install"
+	"github.com/gogf/gf-cli/commands/mod"
 	"github.com/gogf/gf-cli/commands/pack"
 	"github.com/gogf/gf-cli/commands/run"
 	"github.com/gogf/gf-cli/commands/swagger"
@@ -23,10 +19,13 @@ import (
 	"github.com/gogf/gf-cli/library/allyes"
 	"github.com/gogf/gf-cli/library/mlog"
 	"github.com/gogf/gf-cli/library/proxy"
+	"github.com/gogf/gf/errors/gerror"
 	"github.com/gogf/gf/os/gbuild"
 	"github.com/gogf/gf/os/gcmd"
+	"github.com/gogf/gf/os/genv"
 	"github.com/gogf/gf/os/gfile"
 	"github.com/gogf/gf/text/gstr"
+	"strings"
 )
 
 const (
@@ -35,44 +34,47 @@ const (
 
 func init() {
 	// Automatically sets the golang proxy for all commands.
+	// 自动为所有命令设置golang代理
 	proxy.AutoSet()
 }
 
 var (
+	// 从字符串的开头去除空白（或其他字符）
 	helpContent = gstr.TrimLeft(`
-USAGE
-    gf COMMAND [ARGUMENT] [OPTION]
+usage
+    gf command [argument] [option] 
 
-COMMAND
-    env        show current Golang environment variables
-    get        install or update GF to system in default...
-    gen        automatically generate go files for ORM models...
-    mod        extra features for go modules...
-    run        running go codes with hot-compiled-like feature...
-    init       create and initialize an empty GF project...
-    help       show more information about a specified command
-    pack       packing any file/directory to a resource file, or a go file...
-    build      cross-building go project for lots of platforms...
-    docker     create a docker image for current GF project...
-    swagger    swagger feature for current project...
-    update     update current gf binary to latest one (might need root/admin permission)
-    install    install gf binary to system (might need root/admin permission)
-    version    show current binary version info
+command
+    env        	显示当前Golang环境变量
+    get        	默认情况下安装或更新GF到系统
+    gen        	为ORM模型自动生成go文件
+    mod        	go模块的额外功能
+    run        	运行具有热编译特性的go代码
+    init  	创建并初始化一个空的GF项目
+    new		创建一个具备基础功能的GF项目
+    help       	显示有关指定命令的详细信息
+    pack       	将任何文件/目录打包到资源文件或go文件...
+    build      	跨平台交叉编译...
+    docker     	为当前GF项目创建docker映像
+    swagger    	当前项目的swagger功能...
+    update     	更新当前gf二进制文件（可能需要root/admin权限）
+    install    	将gf二进制文件安装到系统（可能需要root/admin权限）
+    version    	显示当前二进制版本信息
 
-OPTION
-    -y         all yes for all command without prompt ask 
-    -?,-h      show this help or detail for specified command
-    -v,-i      show version information
+option
+    -y         all yes for all命令，无需提示询问 
+    -?,-h      显示指定命令的帮助或详细信息 
+    -v,-i      显示版本信息
 
-ADDITIONAL
-    Use 'gf help COMMAND' or 'gf COMMAND -h' for detail about a command, which has '...' 
-    in the tail of their comments.
+additional(额外的)
+    使用“gf help command”或“gf command-h”了解有关命令的详细信息，这些命令的注释末尾有“…”
 `)
 )
 
 func main() {
 	// Force using configuration file in current working directory.
-	genv.Set("GF_GCFG_PATH", gfile.Pwd())
+	// 强制使用当前工作目录中的配置文件
+	genv.Set("GF_GCFG_PATH", gfile.Pwd()) //当前工作目录的绝对路径
 
 	defer func() {
 		if exception := recover(); exception != nil {
@@ -98,9 +100,9 @@ func main() {
 	case "version":
 		version()
 	case "env":
-		env.Run()
+		env.Run() // 执行env命令的逻辑
 	case "get":
-		get.Run()
+		get.Run() // 执行get命令的逻辑
 	case "gen":
 		gen.Run()
 	case "fix":
@@ -109,6 +111,8 @@ func main() {
 		mod.Run()
 	case "init":
 		initialize.Run()
+	case "new":
+		initialize.CreateApp()
 	case "pack":
 		pack.Run()
 	case "docker":
@@ -135,12 +139,13 @@ func main() {
 			}
 		}
 		// No argument or option, do installation checks.
+		// 没有参数或选项，执行安装检查
 		if !install.IsInstalled() {
-			mlog.Print("hi, it seams it's the first time you installing gf cli.")
-			s := gcmd.Scanf("do you want to install gf binary to your system? [y/n]: ")
+			mlog.Print("你好，这是你第一次安装gf cli")
+			s := gcmd.Scanf("是否要在系统中安装gf二进制文件? [y/n]: ")
 			if strings.EqualFold(s, "y") {
 				install.Run()
-				gcmd.Scan("press <Enter> to exit...")
+				gcmd.Scan("按<Enter>退出...")
 				return
 			}
 		}
@@ -149,13 +154,14 @@ func main() {
 }
 
 // help shows more information for specified command.
+// 显示指定命令的详细信息(这个函数封装得很奈斯)
 func help(command string) {
 	switch command {
 	case "get":
 		get.Help()
 	case "gen":
 		gen.Help()
-	case "init":
+	case "new","init":
 		initialize.Help()
 	case "docker":
 		docker.Help()
@@ -183,7 +189,7 @@ func version() {
 	mlog.Printf(`GoFrame CLI Tool %s, https://goframe.org`, VERSION)
 	mlog.Printf(`Install Path: %s`, gfile.SelfPath())
 	if info["gf"] == "" {
-		mlog.Print(`Current is a custom installed version, no installation info.`)
+		mlog.Print(`当前版本为自定义安装版本，没有安装信息.`)
 		return
 	}
 
