8f86139a026006ffcf1bb4dd55e20f6b034205a0
LO2016 <56008171+kotlin2018@users.noreply.github.com>
2021-02-01 13:12:21 +0800

    update getProjectPath() function
    新增 getProjectPath()函数

--- commands/initialize/initialize.go
+++ commands/initialize/initialize.go
@@ -24,78 +24,86 @@ var (
 
 func init() {
 	if cdnUrl == "" {
-		mlog.Fatal("CDN configuration cannot be empty")
+		mlog.Fatal("cdn配置不能为空")
 	}
 	if homeUrl == "" {
-		mlog.Fatal("Home configuration cannot be empty")
+		mlog.Fatal("home配置不能为空")
 	}
 }
 
 func Help() {
 	mlog.Print(gstr.TrimLeft(`
-USAGE    
-    gf init NAME
+usage    
+    gf new projectName
 
-ARGUMENT 
-    NAME  name for the project. It will create a folder with NAME in current directory.
-          The NAME will also be the module name for the project.
+argument 
+    projectName  项目的名称。它将在当前目录中创建一个 name 的文件夹.
+				 projectName也将是项目的模块名称.
 
-EXAMPLES
-    gf init my-app
-    gf init my-project-name
+examples 
+    gf new my-app
+    gf new my-project-name
 `))
 }
 
 func Run() {
-	parser, err := gcmd.Parse(nil)
-	if err != nil {
-		mlog.Fatal(err)
-	}
-	projectName := parser.GetArg(2)
-	if projectName == "" {
-		mlog.Fatal("project name should not be empty")
-	}
+
+	projectName := getProjectPath()
 	dirPath := projectName
-	if !gfile.IsEmpty(dirPath) && !allyes.Check() {
-		s := gcmd.Scanf(`the folder "%s" is not empty, files might be overwrote, continue? [y/n]: `, projectName)
-		if strings.EqualFold(s, "n") {
-			return
-		}
-	}
-	mlog.Print("initializing...")
-	// MD5 retrieving.
+	mlog.Print("初始化...")
+	// --------以下这段逻辑就是从远程服务器地址 "homeUrl + "/cli/project" 获取go-empty项目，并用projectName替换go-empty----------
+	// MD5 检索.
 	respMd5, err := ghttp.Get(homeUrl + "/cli/project/md5")
 	if err != nil {
-		mlog.Fatalf("get the project zip md5 failed: %s", err.Error())
+		mlog.Fatalf("获取项目zip md5失败: %s", err.Error())
 	}
 	defer respMd5.Close()
 	md5DataStr := respMd5.ReadAllString()
 	if md5DataStr == "" {
-		mlog.Fatal("get the project zip md5 failed: empty md5 value. maybe network issue, try again?")
+		mlog.Fatal("获取项目zip md5失败：md5值为空。可能是网络问题，请重试?")
 	}
 
-	// Zip data retrieving.
+	// 压缩数据检索.
 	respData, err := ghttp.Get(cdnUrl + "/cli/project/zip?" + md5DataStr)
 	if err != nil {
-		mlog.Fatal("got the project zip data failed: %s", err.Error())
+		mlog.Fatal("获取项目zip数据失败: %s", err.Error())
 	}
 	defer respData.Close()
 	zipData := respData.ReadAll()
 	if len(zipData) == 0 {
-		mlog.Fatal("get the project data failed: empty data value. maybe network issue, try again?")
+		mlog.Fatal("获取项目数据失败：数据值为空。可能是网络问题，请重试?")
 	}
 
-	// Unzip the zip data.
+	// 解压压缩数据.
 	if err = gcompress.UnZipContent(zipData, dirPath, emptyProjectName+"-master"); err != nil {
-		mlog.Fatal("unzip project data failed,", err.Error())
+		mlog.Fatal("解压缩项目数据失败,", err.Error())
 	}
-	// Replace project name.
+	// 替换项目名称.
 	if err = gfile.ReplaceDir(emptyProject, projectName, dirPath, "Dockerfile,*.go,*.MD,*.mod", true); err != nil {
-		mlog.Fatal("content replacing failed,", err.Error())
+		mlog.Fatal("内容替换失败,", err.Error())
 	}
 	if err = gfile.ReplaceDir(emptyProjectName, projectName, dirPath, "Dockerfile,*.go,*.MD,*.mod", true); err != nil {
-		mlog.Fatal("content replacing failed,", err.Error())
+		mlog.Fatal("内容替换失败,", err.Error())
 	}
-	mlog.Print("initialization done! ")
-	mlog.Print("you can now run 'gf run main.go' to start your journey, enjoy!")
+	mlog.Print("初始化完成! ")
+	mlog.Print("你现在可以在当前目录下运行 'gf run main.go' 来启动项目!")
+}
+
+func getProjectPath()string {
+	parser, err := gcmd.Parse(nil) // 创建并返回一个新的解析器
+	if err != nil {
+		mlog.Fatal(err)
+	}
+	projectName := parser.GetArg(2) //解析器获取第二个参数
+	if projectName == "" {
+		mlog.Fatal("项目名称不应为空")
+	}
+	dirPath := projectName
+	if !gfile.IsEmpty(dirPath) && !allyes.Check() {
+		s := gcmd.Scanf(`文件夹“%s”不为空，文件可能已重写，是否继续? [y/n]: `, projectName)
+		if strings.EqualFold(s, "n") {
+			return ""
+		}
+	}
+	return dirPath
 }
